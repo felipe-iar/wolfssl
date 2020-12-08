19d088d6935525313ae73edf553490110cdf7d0f
Felipe Torrezan <felipe.torrezan@iar.com>
2020-12-08 13:30:57 +0100

    Setup GPT as a FRC

--- IDE/IAR-EWARM/Projects/benchmark_RA6M3/ra_gen/hal_data.c
+++ IDE/IAR-EWARM/Projects/benchmark_RA6M3/ra_gen/hal_data.c
@@ -63,7 +63,7 @@ const gpt_extended_cfg_t g_timer0_extend =
 const timer_cfg_t g_timer0_cfg =
 {
     .mode                = TIMER_MODE_PERIODIC,
-    /* Actual period: 1 seconds. Actual duty: 50%. */ .period_counts = (uint32_t) 0x7270e00, .duty_cycle_counts = 0x3938700, .source_div = (timer_source_div_t)0,
+    /* Actual period: 35.791394133333334 seconds. Actual duty: 50%. */ .period_counts = (uint32_t) 0x100000000, .duty_cycle_counts = 0x80000000, .source_div = (timer_source_div_t)0,
     .channel             = 0,
     .p_callback          = g_timer0_callback,
     .p_context           = NULL,
